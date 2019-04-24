6ec389965332f5c4d55419d8b84c55a6bea45a45
Navid Shamszadeh <nshamszadeh@gmail.com>
2019-04-24 13:21:11 -0700

    Changes to be committed: 	modified:   app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ricculxqdypnfh:d8283cc0c6d1c05d5874a972d5176b29c24751188711916086c6e4537f035274@ec2-23-21-136-232.compute-1.amazonaws.com:5432/dfuo44q4pq80o6'
+db = SQLAlchemy(app)
+
+# Create our database model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
 
 @app.route('/')
 def homepage():
