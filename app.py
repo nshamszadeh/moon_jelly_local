dcb0cc6db3168fc04a936a04e0e6d4afbdf69a84
Navid Shamszadeh <nshamszadeh@gmail.com>
2019-04-25 14:34:42 -0700

    Changes to be committed: 	modified:   app.py 	modified:   logs 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -1,64 +1,43 @@
 from flask import Flask, render_template
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = '    postgres://wktppibqpzzfnv:9986d2db122c2b8209aca8b727ac9cace056f68c08e3f6169caca7a773820cef@ec2-50-17-227-28.compute-1.amazonaws.com:5432/d54m05ksh0rmq4'
 db = SQLAlchemy(app)
 
+# Reusable Web Form
+class ReusableForm(Form):
+  name = TextField('Name:', validators =  [validators.required()])
+
 
 # Create our database model
 class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+  __tablename__ = "users"
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, email):
-        self.email = email
+def __init__(self, email):
+  self.email = email
 
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
+def __repr__(self):
+  return '<E-mail %r>' % self.email
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %I:%M %p")
+  form = ReusableForm(request.form)
+  the_time = datetime.now().strftime("%A, %d %b %Y %I:%M %p")
+  print form.errors
+  if request.method == 'POST':
+    name = request.form['name']
+    print name
 
-# add html code here
-    return render_template('index.html', time=the_time)
 
-@app.route('/grid')
-def grid():
+# add html file here
+  return render_template('index.html', time=the_time, form = form)
 
-# add html code here
-    return """
-    <title>Example</title>
-	<style>
-	#grid { 
-  		display: grid;
-  		grid-template-rows: 1fr 1fr 1fr;
-  		grid-template-columns: 1fr 1fr 1fr;
-  		grid-gap: 2vw;
-  	}
-	#grid > div {
-  		font-size: 5vw;
-  		padding: .5em;
-  		background: gold;
-  		text-align: center;
-	}
-	</style>
-	<div id="grid">
-  		<div>1</div>
-  		<div>2</div>
-  		<div>3</div>
-  		<div>4</div>
-  		<div>5</div>
-  		<div>6</div>
-  		<div>7</div>
-  		<div>8</div>
-  		<div>9</div>
-	</div>
-"""
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+  app.run(debug=True, use_reloader=True)
