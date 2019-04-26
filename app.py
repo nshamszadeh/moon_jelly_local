fd8e4ef9d6e21248447d441181a6e94ff1374ad9
lucqiiiii <40619027+lucqiiiii@users.noreply.github.com>
2019-04-26 00:23:59 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -32,14 +32,19 @@ class User(db.Model):
     self.last_name = last_name
     self.specialty = specialty
 
+#This is the homepage with navbar
+#@app.route('/')
+#def homepage():
+  
+
 # This is the main homepage for now. GET and POST are for web forms.
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/register', methods = ['GET', 'POST'])
 def homepage():
   
   # define a form object
   user_form = UserForm()
 
-  # if we are posting a form, i.e. submitting a form, store all the info in these variables
+  if we are posting a form, i.e. submitting a form, store all the info in these variables
   if request.method == 'POST':
     first_name = request.form['first_name'] 
     last_name = request.form['last_name']
@@ -48,7 +53,7 @@ def homepage():
 
     # if the inputs we're all validated by WTforms (improve validation later)
     if user_form.validate(): 
-      # then store info in an initialized User object and store the object in the database
+      then store info in an initialized User object and store the object in the database
       new_user = User(email, first_name, last_name, specialty)
       db.session.add(new_user) # add to database
       db.session.commit() # for some reason we also need to commit it otherwise it won't add
@@ -56,8 +61,8 @@ def homepage():
       print("Invalid input(s)!")
 
   # add html file here
-  return render_template('index.html', form = user_form)
-
+ return render_template('home.html', form = user_form)
+ #return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
