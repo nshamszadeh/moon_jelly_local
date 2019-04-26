39f345c7638a63d654febd11f34862e376b38267
Navid Shamszadeh <nshamszadeh@gmail.com>
2019-04-25 20:53:06 -0700

    Changes to be committed: 	modified:   __pycache__/app.cpython-37.pyc 	modified:   app.py 	modified:   logs 	modified:   requirements.txt 	modified:   templates/index.html
     Untracked files:
    	__pycache__/forms.cpython-37.pyc

--- app.py
+++ app.py
@@ -1,42 +1,62 @@
-from flask import Flask, render_template
-from datetime import datetime
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from forms import UserForm
+
+# Some boilerplate setup stuff.
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = '    postgres://wktppibqpzzfnv:9986d2db122c2b8209aca8b727ac9cace056f68c08e3f6169caca7a773820cef@ec2-50-17-227-28.compute-1.amazonaws.com:5432/d54m05ksh0rmq4'
-db = SQLAlchemy(app)
+# URL should be whatever database URL is being used (if testing on your own use a database different from the team's )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wktppibqpzzfnv:9986d2db122c2b8209aca8b727ac9cace056f68c08e3f6169caca7a773820cef@ec2-50-17-227-28.compute-1.amazonaws.com:5432/d54m05ksh0rmq4'
+app.config['SECRET_KEY'] = 'mOon_jElLy wAs oRiGiNa11y g0nNa b3 SuP3r MaRi0 gAlAxY' # need to change later
+# im not mocking Aidan, this key actually needs to be secure which is why it looks all crazy
 
-# Reusable Web Form
-class ReusableForm(Form):
-  name = TextField('Name:', validators =  [validators.required()])
+db = SQLAlchemy(app) # wow we have a database
 
-
-# Create our database model
+# Create our database model. 
 class User(db.Model):
+
   __tablename__ = "users"
+
+  # Each user (doctor) will have all these things attributed to him or her
   id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(120), unique=True)
+  email = db.Column(db.Text, unique=True)
+  first_name = db.Column(db.Text)
+  last_name = db.Column(db.Text)
+  specialty = db.Column(db.Text)
 
-def __init__(self, email):
-  self.email = email
+  # initialize the object
+  def __init__(self, email, first_name, last_name, specialty):
+    self.email = email
+    self.first_name = first_name
+    self.last_name = last_name
+    self.specialty = specialty
 
-def __repr__(self):
-  return '<E-mail %r>' % self.email
-
+# This is the main homepage for now. GET and POST are for web forms.
 @app.route('/', methods = ['GET', 'POST'])
 def homepage():
-  form = ReusableForm(request.form)
-  the_time = datetime.now().strftime("%A, %d %b %Y %I:%M %p")
-  print (form.errors)
+  
+  # define a form object
+  user_form = UserForm()
+
+  # if we are posting a form, i.e. submitting a form, store all the info in these variables
   if request.method == 'POST':
-    name = request.form['name']
-    print (name)
+    first_name = request.form['first_name'] 
+    last_name = request.form['last_name']
+    email = request.form['email']
+    specialty = request.form['specialty']
 
+    # if the inputs we're all validated by WTforms (improve validation later)
+    if user_form.validate(): 
+      # then store info in an initialized User object and store the object in the database
+      new_user = User(email, first_name, last_name, specialty)
+      db.session.add(new_user) # add to database
+      db.session.commit() # for some reason we also need to commit it otherwise it won't add
+    else:
+      print("Invalid input(s)!")
 
-# add html file here
-  return render_template('index.html', time=the_time, form = form)
+  # add html file here
+  return render_template('index.html', form = user_form)
 
 
 if __name__ == '__main__':
