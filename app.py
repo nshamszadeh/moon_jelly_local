0c86fc6568d13af51a1ec575ec354cb16d279266
lucqiiiii <40619027+lucqiiiii@users.noreply.github.com>
2019-04-26 00:17:59 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,108 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from forms import UserForm
+
+# Some boilerplate setup stuff.
 
 app = Flask(__name__)
 
-@app.route('/')
+# URL should be whatever database URL is being used (if testing on your own use a database different from the team's )
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ricculxqdypnfh:d8283cc0c6d1c05d5874a972d5176b29c24751188711916086c6e4537f035274@ec2-23-21-136-232.compute-1.amazonaws.com:5432/dfuo44q4pq80o6'
+app.config['SECRET_KEY'] = 'mOon_jElLy wAs oRiGiNa11y g0nNa b3 SuP3r MaRi0 gAlAxY' # need to change later
+# im not mocking Aidan, this key actually needs to be secure which is why it looks all crazy
 
-def index():
-	return render_template('home.html')
+db = SQLAlchemy(app) # wow we have a database
+
+# Create our database model. 
+class User(db.Model):
+
+  __tablename__ = "users"
+
+  # Each user (doctor) will have all these things attributed to him or her
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.Text, unique=True)
+  first_name = db.Column(db.Text)
+  last_name = db.Column(db.Text)
+  specialty = db.Column(db.Text)
+
+  # initialize the object
+  def __init__(self, email, first_name, last_name, specialty):
+    self.email = email
+    self.first_name = first_name
+    self.last_name = last_name
+    self.specialty = specialty
+
+# This is the main homepage for now. GET and POST are for web forms.
+@app.route('/', methods = ['GET', 'POST'])
+def homepage():
+  
+  # define a form object
+  user_form = UserForm()
+
+  # if we are posting a form, i.e. submitting a form, store all the info in these variables
+  if request.method == 'POST':
+    first_name = request.form['first_name'] 
+    last_name = request.form['last_name']
+    email = request.form['email']
+    specialty = request.form['specialty']
+
+    # if the inputs we're all validated by WTforms (improve validation later)
+    if user_form.validate(): 
+      # then store info in an initialized User object and store the object in the database
+      new_user = User(email, first_name, last_name, specialty)
+      db.session.add(new_user) # add to database
+      db.session.commit() # for some reason we also need to commit it otherwise it won't add
+    else:
+      print("Invalid input(s)!")
+
+  # add html file here
+  return render_template('index.html', form = user_form)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+  app.run(debug=True, use_reloader=True)
+
+
+
+"""
+
+from flask import Flask
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
+migrate=Migrate(app,db)
+# Create our database model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, email):
+        self.email = email
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %I:%M %p")
+
+# add html code here
+return 
+
+
+
+@app.route('/users')
+def users():
+  u = User.query.all()
+  return '<br/>'.join([a.email for a in u])
+
+if __name__ == '__main__':
+app.run(debug=True, use_reloader=True)
+
+
+"""
